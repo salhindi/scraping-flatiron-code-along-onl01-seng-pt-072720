70f157a4e2e1b1c1386aa0072389032fda3362a8
Sara A <calisaraalhindi@gmail.com>
2020-09-09 00:39:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,6 +32,7 @@ class Scraper
   
   def make_courses
     #responsible for instantiating Course objects and giving each course object the correct title/schedule/description attr. that we scraped from the page.
+  end
 
 end
 
