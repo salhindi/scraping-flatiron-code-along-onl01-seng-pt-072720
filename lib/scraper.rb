123b87b22bb92e5abc02c1e80d191af4f5a61f30
Sara A <calisaraalhindi@gmail.com>
2020-09-09 00:50:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,16 @@ require_relative './course.rb'
 
 class Scraper
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
@@ -24,16 +24,16 @@ class Scraper
   end
   Scraper.new.get_page
   
-  def get_courses
-    # using a CSS selector to grab all of the HTML elements that contain a course. 
+  # def get_courses
+  #   # using a CSS selector to grab all of the HTML elements that contain a course. 
     
-    #return value should = collection of Nokogiri XML elements
-     #-each describes course offering (examine page with element inspector)
-  end
+  #   #return value should = collection of Nokogiri XML elements
+  #   #-each describes course offering (examine page with element inspector)
+  # end
   
-  def make_courses
-    #responsible for instantiating Course objects and giving each course object the correct title/schedule/description attr. that we scraped from the page.
-  end
+  # def make_courses
+  #   #responsible for instantiating Course objects and giving each course object the correct title/schedule/description attr. that we scraped from the page.
+  # end
 
 end
 
